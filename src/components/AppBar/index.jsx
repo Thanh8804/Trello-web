1bb418cfe44b7fd2378c24ed4accb62629770d8b
12_7-Đặng Chí Thành <chithanh080804@gmail.com>
2024-02-03 17:52:36 +0700

    code thanh BoardBar

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -16,6 +16,7 @@ import Tooltip from '@mui/material/Tooltip'
 import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone'
 import HelpOutlineIcon from '@mui/icons-material/HelpOutline'
 import Profiles from './Menus/Profiles'
+import LibraryAddIcon from '@mui/icons-material/LibraryAdd'
 
 function AppBar() {
   return (
@@ -42,7 +43,7 @@ function AppBar() {
           <Recent />
           <Starred />
           <Templates />
-          <Button variant="outlined">Outlined</Button>
+          <Button variant="outlined" startIcon ={<LibraryAddIcon />}> Create </Button>
         </Box>
       </Box>
       <Box sx ={{ display: 'flex', alignItems: 'center', gap: 2, padding: 0 }}>
